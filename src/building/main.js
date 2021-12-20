e0b45c3875f3f98276d54217bfd8745641fc173b
NSVEGUR <nsvegur@gmail.com>
2021-12-20 23:06:30 +0530

    modified blog core design

--- src/building/main.js
+++ src/building/main.js
@@ -1,1 +1,6 @@
 import './style.css'
+
+document.getElementById('float').addEventListener('click', () => {
+	document.querySelector('.like').classList.toggle('hidden');
+	document.querySelector('.liked').classList.toggle('hidden');
+})
