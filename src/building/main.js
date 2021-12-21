74843e5266ff10123ec514bcc9d0c6a86aaf6b84
NSVEGUR <nsvegur@gmail.com>
2021-12-22 00:52:46 +0530

    added horizontal progress bar to blogs

--- src/building/main.js
+++ src/building/main.js
@@ -1,6 +1,18 @@
 import './style.css'
 
+let progress = document.getElementById('horizontalProgressBar');
+let totalHeight = document.body.scrollHeight - window.innerHeight;
+
+window.addEventListener('scroll', () => {
+	let progressHeight = (window.pageYOffset / totalHeight) * 100;
+	progress.style.width = progressHeight + '%';
+})
+
 document.getElementById('float').addEventListener('click', () => {
 	document.querySelector('.like').classList.toggle('hidden');
 	document.querySelector('.liked').classList.toggle('hidden');
+})
+document.getElementById('float-mode').addEventListener('click', () => {
+	document.querySelector('.light').classList.toggle('hidden');
+	document.querySelector('.dark').classList.toggle('hidden');
 })
