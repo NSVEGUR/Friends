7bac365385617127a11165b8608a5c40d66b366d
NSVEGUR <nsvegur@gmail.com>
2021-12-25 00:08:10 +0530

    configured the path for templates

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,12 @@ export default defineConfig({
 				main: resolve(root, 'index.html'),
 				apartment: resolve(root, 'apartment', 'index.html'),
 				weirdone: resolve(root, 'weirdone', 'index.html'),
+				lobsters: resolve(root, 'lobsters', 'index.html'),
+				foodie: resolve(root, 'foodie', 'index.html'),
+				centralperk: resolve(root, 'centralperk', 'index.html'),
+				thanksgiving: resolve(root, 'thanksgiving', 'index.html'),
+				seasons: resolve(root, 'seasons', 'index.html'),
+				mondler: resolve(root, 'mondler', 'index.html'),
 			}
 		}
 	}
